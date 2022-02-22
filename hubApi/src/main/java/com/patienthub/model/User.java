b72dda4973110d39b41a13d23a9e42e8b9649414
tumisé <oladele.alade@gmail.com>
2022-02-22 21:04:20 +0000

    chsetup user model

---
+++ hubApi/src/main/java/com/patienthub/model/User.java
@@ -0,0 +1,111 @@
+package com.patienthub.model;
+
+import javax.validation.constraints.NotNull;
+
+public class User {
+
+    @NotNull(message = "first name is required")
+    private String firstName;
+
+    @NotNull(message = "last name is required")
+    private String lastName;
+
+    @NotNull(message = "contact number is required")
+    private String contactNum;
+
+    @NotNull(message = "email is required")
+    private String email;
+
+    @NotNull(message = "pps is required")
+    private String pps;
+
+    private String gender = "others";
+
+    private String role;
+
+    private boolean active;
+
+    public User() {
+
+    }
+
+    public User(@NotNull(message = "first name is required") String firstName,
+            @NotNull(message = "last name is required") String lastName,
+            @NotNull(message = "contact number is required") String contactNum,
+            @NotNull(message = "email is required") String email, @NotNull(message = "pps is required") String pps,
+            String gender, String role, boolean active) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.contactNum = contactNum;
+        this.email = email;
+        this.pps = pps;
+        this.gender = gender;
+        this.role = role;
+        this.active = active;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getContactNum() {
+        return contactNum;
+    }
+
+    public void setContactNum(String contactNum) {
+        this.contactNum = contactNum;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPps() {
+        return pps;
+    }
+
+    public void setPps(String pps) {
+        this.pps = pps;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+}
