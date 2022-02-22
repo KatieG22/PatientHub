878e5163562e96b93827953e4e3409824e40a3f6
DESKTOP-TSR3IQQ\Asus Touch <mungaiglen79@gmail.com>
2022-02-22 18:42:13 +0000

    Added admin, staff and patients db scripts

--- hubApi/src/main/resources/users.sql
+++ hubApi/src/main/resources/users.sql
@@ -14,6 +14,28 @@ create table hAdmin(
 id int primary key,
 pps varchar(15) not null,
 eirCode VARCHAR(10) NOT NULL,
- FOREIGN KEY (pps) REFERENCES users(pps),
+ constraint hospAdmin FOREIGN KEY (pps) REFERENCES users(pps),
   FOREIGN KEY (eirCode) REFERENCES hospital(eirCode)
-)
+  on delete cascade
+);
+
+drop table if exists hStaff;
+create table hStaff(
+pps varchar(15) not null,
+staffId varchar (15) not null,
+eirCode VARCHAR(10) NOT NULL,
+speciality varchar(15) not null,
+constraint hospStaff FOREIGN KEY (pps) REFERENCES users(pps),
+FOREIGN KEY (eirCode) REFERENCES hospital(eirCode)
+on delete cascade
+);
+
+drop table if exists hPatient;
+create table hPatient(
+pps varchar(15) not null,
+patientId varchar(15) not null,
+eirCode varchar(10) not null,
+constraint hospPatient FOREIGN KEY (pps) REFERENCES users(pps),
+FOREIGN KEY (eirCode) REFERENCES hospital(eirCode)
+on delete cascade
+);
